eac95e866150dce5e75c1c96293867aaeec5d8f0
todos-n <nina.todos@gmail.com>
2021-05-29 21:00:26 +0300

    Parametrs to test_guest_can_add_product_to_basket added

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
 from pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, promo):
+    
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
