c18252ad5bafeb22bb8e25a6d562b61b416f29ed
todos-n <nina.todos@gmail.com>
2021-05-29 22:31:09 +0300

    Negative tests added

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,20 +11,20 @@ class ProductPage(BasePage):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
         #Обработать alert
         self.solve_quiz_and_get_code()
-        #есть ли сообщение о добавлении
-        self.should_be_adding_message()
-        #название совпадает с добавленным
-        self.should_be_the_right_name()
-        #есть ли сообщение со стоимостью корзины
-        self.should_be_sum_message()
-        #стоимость совпадает с ценой
-        self.should_be_the_right_price()
+        #есть ли сообщение о добавлении (перенесла в тест)
+        #self.should_be_success_message()
+        #название совпадает с добавленным (перенесла в тест)
+        #self.should_be_the_right_name()
+        #есть ли сообщение со стоимостью корзины (перенесла в тест)
+        #self.should_be_sum_message() 
+        #стоимость совпадает с ценой (перенесла в тест)
+        #self.should_be_the_right_price()
 
     def should_be_add_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Add to basket button is not presented"
 
-    def should_be_adding_message(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_NAME), "Adding message is not presented"
+    def should_be_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_NAME), "Success message is not presented"
 
     def should_be_the_right_name(self):
         assert self.browser.find_element(*ProductPageLocators.MESSAGE_NAME).text == self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text, "Wrong product name in the message"
@@ -34,3 +34,10 @@ class ProductPage(BasePage):
 
     def should_be_the_right_price(self):
         assert self.browser.find_element(*ProductPageLocators.MESSAGE_SUM).text == self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text, "Wrong sum in the message"
+
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_NAME), "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_NAME), "Success message is presented, but should disappear"
--- test_product_page.py
+++ test_product_page.py
@@ -3,9 +3,36 @@ import pytest
 
 @pytest.mark.parametrize('promo', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 def test_guest_can_add_product_to_basket(browser, promo):
+#def test_guest_can_add_product_to_basket(browser):
     
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    
+    page.should_be_success_message()
+    page.should_be_the_right_name()
+    page.should_be_sum_message()
+    page.should_be_the_right_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_disappeared_success_message()
